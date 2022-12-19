75c451ee0fdaaeda2d4a07e42a5c7dcc4a8819f7
M3R14M <meriamvangalen@gmail.com>
2022-12-19 14:05:15 +0100

    finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+let titles = books.map((key)=>key.title);
+return titles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+//npm test getTheTitles.spec.js
