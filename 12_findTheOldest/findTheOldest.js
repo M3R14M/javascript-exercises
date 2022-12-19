1088e3c301f7f7d3df2cd0afc44b10204f2fca3c
M3R14M <meriamvangalen@gmail.com>
2022-12-19 14:32:24 +0100

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) =>{
+        let oldAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let age = getAge(person.yearOfBirth, person.yearOfDeath);
+        if (oldAge < age) {
+            return person;
+        }
+        else {
+            return oldest;
+        };
+    });
+};
 
+let getAge = function(birthYear, deathYear){
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+//npm test findTheOldest.spec.js
