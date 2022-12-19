fe7ab5040e8abcab14866f0ef31e4900c8eaf980
M3R14M <meriamvangalen@gmail.com>
2022-12-19 13:18:39 +0100

    finished exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+let string = input.toLowerCase().replace(/[^a-z]/g, ''); //remove capitalization, and anything that isn't a lowercase letter
+return (
+    string
+        .split('') //splits string into array
+        .reverse() // reverses array
+        .join('') === string //joins array and checks if reversed string matches original string
+        ); //returns either false or true
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+//npm test palindromes.spec.js
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+if (n == NaN){
+    return parseInt(n, 10);
+};
+
+let a = 0, b = 1, c, i;
+for (i=0; i<n;i++){
+    c = a + b;
+    a = b;
+    b = c;
+};
+if (n < 0){ //if negative number
+    return "OOPS";
+}
+else {
+    return a;
+};
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+//npm test fibonacci.spec.js
